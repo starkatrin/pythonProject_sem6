b6438b436175ae9bdb12d7af3148f30778540993
Acer <starkatrin0702@gmail.com>
2022-11-16 15:21:37 +0500

    Добавление вычисления в скобках

--- calc.py
+++ calc.py
@@ -13,7 +13,10 @@ def list_complex(inp_str):
         if inp_str[i] == '(':
             while inp_str[i] != ')':
                 i += 1
-            res.append(complex(inp_str[n:i+1]))
+            try:
+                res.append(complex(inp_str[n:i+1]))
+            except:
+                res.append(list_complex(inp_str[n+1:i]))
         n = i + 1
         if n >= len(inp_str):
             return res
@@ -27,40 +30,43 @@ def calculator(calc_list):
         return calc_list[0]
     for sign in ('^', '*','/','-','+'):
         for i in range(len(calc_list)):
-            if calc_list[i] == sign:
-                left, op, right = calc_list[i-1], calc_list[i], calc_list[i+1]
-                if op == '^':
-                    ans = (left) ** (right)
-                    calc_list[i] = ans
-                    calc_list.remove(left)
-                    calc_list.remove(right)
-                    return calculator(calc_list)
-                elif op == '*':
-                    ans = (left) * (right)
-                    calc_list[i] = ans
-                    calc_list.remove(left)
-                    calc_list.remove(right)
-                    return calculator(calc_list)
-                elif op == '/':
-                    ans = (left) / (right)
-                    calc_list[i] = ans
-                    calc_list.remove(left)
-                    calc_list.remove(right)
-                    return calculator(calc_list)
-                elif op == '+':
-                    ans = (left) + (right)
-                    calc_list[i] = ans
-                    calc_list.remove(left)
-                    calc_list.remove(right)
-                    return calculator(calc_list)
-                elif op == '-':
-                    ans = (left) - (right)
-                    calc_list[i] = ans
-                    calc_list.remove(left)
-                    calc_list.remove(right)
-                    return calculator(calc_list)
+            if type(calc_list[i]) == list:
+                calc_list[i] = calculator(calc_list[i])
+            else:
+                if calc_list[i] == sign:
+                    left, op, right = calc_list[i-1], calc_list[i], calc_list[i+1]
+                    if op == '^':
+                        ans = (left) ** (right)
+                        calc_list[i] = ans
+                        calc_list.remove(left)
+                        calc_list.remove(right)
+                        return calculator(calc_list)
+                    elif op == '*':
+                        ans = (left) * (right)
+                        calc_list[i] = ans
+                        calc_list.remove(left)
+                        calc_list.remove(right)
+                        return calculator(calc_list)
+                    elif op == '/':
+                        ans = (left) / (right)
+                        calc_list[i] = ans
+                        calc_list.remove(left)
+                        calc_list.remove(right)
+                        return calculator(calc_list)
+                    elif op == '+':
+                        ans = (left) + (right)
+                        calc_list[i] = ans
+                        calc_list.remove(left)
+                        calc_list.remove(right)
+                        return calculator(calc_list)
+                    elif op == '-':
+                        ans = (left) - (right)
+                        calc_list[i] = ans
+                        calc_list.remove(left)
+                        calc_list.remove(right)
+                        return calculator(calc_list)
 
-inp_str = '(2.3+5j)+3*(5+7j)-6*5.1'
+inp_str = '(2.3+5j)*2+3*(5+7j)-6*5.1'
 calc_list = list_complex(inp_str)
 answer = calculator(calc_list)
 print(answer)
